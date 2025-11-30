9a598b59b2f3e87630eea87b638323ec7af1b8d9
Lawremwashail <lawremwash@gmail.com>
2025-11-30 11:17:08 +0300

    Fix GraphQL GraphiQL static URL and update urls.py

--- social-media-backend/core/urls.py
+++ social-media-backend/core/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
+# For serving static files in development
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
+
+# Serve static files when DEBUG=True
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
